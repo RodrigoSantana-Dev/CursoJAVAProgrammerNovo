d97c6dbc34dae20e3ed272dbecb6d392d6c5cab9
leite santana <Rodrigo_l.santana20@hotmail.com>
2024-10-20 20:12:20 -0300

    Cadastro de Aluno

--- Aula-02-PowerShell/MeuPrimeiroApp/Principal.java
+++ Aula-02-PowerShell/MeuPrimeiroApp/Principal.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 class principal {
 
 	public static void main (String [] args){
 
-	System.out.println("Meu Primeiro Commit");
+	System.out.println("Digite seu nome:");
+	
+	Scanner entradaDados = new Scanner(System.in);
+	String nome = entradaDados.next();
+
+
+	System.out.println("Digite sua Idade:");
+	String Idade = entradaDados.next();
+
+	System.out.println("Digite seu endereco:");
+	String endereco = entradaDados.next();
+
+	System.out.println("O nome digitado foi: " + nome  );
+	System.out.println("A Idade digitada foi: " + Idade );
+	System.out.println("O endereco digitado foi: " + endereco );
+	
+
+	
+	
 
 
    }
